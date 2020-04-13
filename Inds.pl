b2e9dae56903401ab4cf98a314be6b3e4a0db8c4
XoneK <mr.blackmarch@mail.ru>
2020-04-13 12:56:12 +0300

    3inda

--- Inds.pl
+++ Inds.pl
@@ -26,3 +26,19 @@ build_new_list(A,B,Am,Max):-b_n_w(A,[],B,Am,Max).
 b_n_w([],B,B,_,_):-!.
 b_n_w([Head|Tail],Z,B,Am,Max):-Head>Am,Head<Max,append(Z,[Head],Z1),b_n_w(Tail,Z1,B,Am,Max),!.
 b_n_w([_|Tail],Z,B,Am,Max):-b_n_w(Tail,Z,B,Am,Max).
+
+read_str(A,N):-get0(X),r_str(X,A,[],N,0).
+r_str(10,A,A,N,N):-!.
+r_str(X,A,B,N,K):- K1 is K+1,append(B,[X],B1), get0(X1),r_str(X1,A,B1,N,K1).
+
+write_str([]):-!.
+write_str([H|T]):-put(H),write_str(T).
+
+ind3:-read_str(A,N),write_str(A),new_list(A,B),write_str(B).
+
+new_list([H|T],B):-Count_el(T,Count,H),delete_el(T,H),append()
+
+Count_el(A,Count,El):-Count_el(A,1,Count,El).
+Count_el([],Count,Count,_):-!.
+Count_el([El|T],Z,Count,El):- Z1 is Z+1,Count_el(T,Z1,Count,El),!.
+Count_el([_|T],Z,Count,El):-Count_el(T,Z,Count,El).
