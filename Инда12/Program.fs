6871ed6de82855df8f46e0e22f6a1eb507f2934c
XoneK <mr.blackmarch@mail.ru>
2020-05-25 00:25:52 +0300

    Сделал 12 инду

--- Инда12/Program.fs
+++ Инда12/Program.fs
@@ -1,7 +1,30 @@
-﻿// Learn more about F# at https://fsharp.org
-// See the 'F# Tutorial' project for more help.
+﻿let rec read_list n = 
+    if n=0 then []
+    else
+    let Head = System.Convert.ToInt32(System.Console.ReadLine())
+    let Tail = read_list (n-1)
+    Head::Tail
+
+let read_data = 
+    printf "Введите кол-во элементов в списке: "
+    let n=System.Convert.ToInt32(System.Console.ReadLine())
+    printf "Введите элементы:\n"
+    read_list n
+
+let solve list = 
+    let rec solve_t sum count = function
+        [] -> count
+        |head::tail -> if(head > sum) then solve_t (sum + head) (count + 1) tail
+                                      else solve_t (sum+head) count tail
+    solve_t 0 0 list
+    
 
 [<EntryPoint>]
 let main argv =
-    printfn "%A" argv
-    0 // return an integer exit code
+   
+    let list = read_data
+    let Ans = solve list
+    printfn "%A" list
+    printfn "\nКол-во элементов, которые больше, чем сумма всех предыдущих = %A" Ans
+    let z = System.Console.ReadKey()
+    0 
