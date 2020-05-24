0d27c749b2629ef7b8257619b0fb45802028e989
XoneK <mr.blackmarch@mail.ru>
2020-05-24 23:29:59 +0300

    Сделал 11 инду

--- Инда11/Program.fs
+++ Инда11/Program.fs
@@ -1,7 +1,27 @@
-﻿// Learn more about F# at https://fsharp.org
-// See the 'F# Tutorial' project for more help.
+﻿open System
+open System.Windows.Forms
+open System.Drawing
 
-[<EntryPoint>]
-let main argv =
-    printfn "%A" argv
-    0 // return an integer exit code
+let form = new Form(Text = "Ind 11", Width = 500, Height = 300)
+let button = new Button(Text = "Стереть все", Width = 150, Height =50)
+let text = new TextBox( Width = 300, Height = 25,MaxLength =50)
+let progress = new ProgressBar(Step = 1, Width =300, Height = 25, Style =ProgressBarStyle.Continuous, Maximum = 50)
+
+text.Location<-new Point(100,100)
+
+progress.Location<-new Point(100,50)
+
+button.Location <- new Point (180,150)
+
+
+form.Controls.Add(button)
+form.Controls.Add(text)
+form.Controls.Add(progress)
+
+let Change _ = progress.Value <- text.Text.Length
+let _ = text.TextChanged.Add(Change)
+
+button.Click.Add(fun evArgs -> text.Clear() |>ignore)
+
+
+Application.Run(form)
